0da375ec8e1ad8308e76e561ba2505ea58ca1aea
rad1k4l <zeynalli169@gmail.com>
2022-04-03 01:42:18 +0400

    add logging functionality

--- core/settings.py
+++ core/settings.py
@@ -174,3 +174,17 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "INFO"},
+    },
+}
