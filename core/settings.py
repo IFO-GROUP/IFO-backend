7177e4e4e9048d9789d1afce782b688f46183e58
Gandabh <qendabhesenova@gmail.com>
2022-04-08 15:52:59 +0400

    EMAIL_HOST_PASSWORD added

--- core/settings.py
+++ core/settings.py
@@ -192,10 +192,11 @@ LOGGING = {
 
 # email details
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST_USER = 'info@ifocorporation.com'
-# EMAIL_HOST_PASSWORD = '0000000000' 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'info@ifocorporation.com'
+EMAIL_HOST_PASSWORD = 'gymdvqrbhsdukpgi' 
+
 
